d9013014e990d9026c42834570498b55083bab4e
lingk4231075 <strbryjam1220@gmail.com>
2024-09-28 18:55:46 +0900

    사각형 구조체 구현

--- CG_w4_opengl_3/CG_w4_opengl_3.cpp
+++ CG_w4_opengl_3/CG_w4_opengl_3.cpp
@@ -2,6 +2,8 @@
 #include <gl/glew.h> 
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
+#include <vector>
+#include <math.h>
 
 
 
@@ -9,15 +11,44 @@ GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 
+std::vector<int> rectangles;
+
+struct rect {
+	GLfloat x1;
+	GLfloat y1;
+	GLfloat x2;
+	GLfloat y2;
+
+	GLclampf r;
+	GLclampf g;
+	GLclampf b;
+	GLclampf a = 0;
+
+
+	void init() {
+
+		GLint viewport[4];
+		glGetIntegerv(GL_VIEWPORT, viewport);
+		int viewport_width = viewport[2];
+		int viewport_height = viewport[3];
+		x1 = (float)(rand() % viewport_width) / viewport_width * 2 - 1.0f;
+		x2 = x1 + 0.1f;
+		y1 = (float)(rand() % viewport_height) / viewport_height * 2 - 1.0f;
+		y2 = y1 + 0.1f;
+	}
+};
+
 void main(int argc, char** argv)
 {
-
+	srand((unsigned int)time(NULL));
+	
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("CG_4w_opengl_3");
+
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -48,9 +79,13 @@ GLvoid Reshape(int w, int h)
 GLvoid Keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
+	case 'a':
+
+		break;
 	case 'q':
 		glutLeaveMainLoop();
 		break;
+
 	}
 	glutPostRedisplay();
 }
